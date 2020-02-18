529dc0ec26ac26e8b27e7c3c154ba6a00a037548
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2020-02-18 16:12:06 +0100

    Add 6th module - Packaging Libraries for Reuse

---
+++ 6.packaging-libraries-for-reuse/Weather/Client/Keys.cs
@@ -0,0 +1,17 @@
+﻿namespace Client
+{
+    using System;
+
+    public static class Keys
+    {
+        public const string OpenWeatherMapApiKey = "834afebb501cb78e11525767b6ba7c3f";
+
+        public static void ThrowIfKeysNotSet()
+        {
+            if (string.IsNullOrWhiteSpace(OpenWeatherMapApiKey))
+            {
+                throw new ArgumentException("You need to add your API keys");
+            }
+        }
+    }
+}
---
+++ 6.packaging-libraries-for-reuse/Weather/Web/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Weather(string city = "Aarhus, DK")
+        {
+            var client = new OpenWeatherMapClient();
+
+            var weather = await client.GetCurrentWeatherByCityAsync(city);
+
+            if (weather is null)
+            {
+                return Content($"No weather for {city}");
+            }
+
+            var message = $"<h2>{city}</h2>"
+                + $"<img src=\"{weather.FirstCondition?.IconUrl}\" /><br>"
+                + $"Condition: {weather.FirstCondition?.Description}<br>"
+                + $"Temp: {weather.Main?.Temperature}<br>"
+                + $"Low: {weather.Main?.MinTemperature}<br>"
+                + $"High: {weather.Main?.MaxTemperature}<br>"
+                + $"Humidity: {weather.Main?.Humidity}%<br>";
+
+            return Content(message, "text/html");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
